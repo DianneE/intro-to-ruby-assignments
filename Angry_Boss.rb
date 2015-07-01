e7962eec8a5dda056819de9b9e23f690f8061e48
Dianne Eramo <dianneeramo@Diannes-MacBook.local>
2015-07-01 12:43:29 -0400

    99 Bottles stage 1

--- Angry_Boss.rb
+++ Angry_Boss.rb
@@ -1,8 +1,6 @@
 puts 'And what do yoooooou want?'
 employ_wants = gets.chomp
 
-puts "WHADDAYA MEAN \""  + employ_wants.upcase + "\"?!? \nYOU\'RE FIRED!!"
-
-
+puts "WHADDAYA MEAN \""  + employ_wants.upcase + "\"?!? \nYOU\'RE FIRED!
 
 
---
+++ Name_ex_3.rb
@@ -0,0 +1,10 @@
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+
+puts 'Hello, ' + name + '.'
+
+if name == 'Dianne'
+	puts 'What a lovely name!'
+elsif name == 'Jeanie'
+	puts 'That\'s a terrible name!'
+end
